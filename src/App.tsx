83a35ddeec3d8a3f94fdb5184227f78659d3193d
janechu <7559015+janechu@users.noreply.github.com>
2023-09-22 15:21:31 -0700

    Update to use webpack instead of create react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
@@ -16,7 +15,6 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header" style={{ backgroundColor }}>
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HTMLWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = env => {
+    return {
+        entry: {
+            main: path.resolve(__dirname, "./src/index.tsx")
+        },
+        mode: "development",
+        resolve: {
+            extensions: [".js", ".ts", ".jsx", ".tsx", ".css"]
+        },
+        devServer: {
+            port: 3000,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', { targets: "defaults" }],
+                                ["@babel/preset-typescript"],
+                                ["@babel/preset-react"]
+                            ],
+                            plugins: ['istanbul']
+                        }
+                    }
+
+                },
+                {
+                    test: /\.(css)$/i,
+                    use: ["style-loader", "css-loader"]
+                }
+            ]
+        },
+        plugins: [
+            new HTMLWebPackPlugin({
+                template: path.resolve(__dirname, "public/index.html")
+            })
+        ]
+    }
+}
